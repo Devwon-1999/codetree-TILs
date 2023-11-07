40cf09a9b14ba7c91101f01e1afe38a234f27803
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-11-08 01:37:19 +0900

    Title: 두 사람, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231107/두 사람/two-person.py
+++ 231107/두 사람/two-person.py
@@ -3,7 +3,7 @@ c, d = input().split()
 a = int(a)
 c = int(c)
 
-if (a >= 19 or c >= 19) and (b == "M" or d =="M"):
+if (a >= 19 or b == "M") and (c >= 19 or d =="M"):
     print(1)
 else:
     print(0)
