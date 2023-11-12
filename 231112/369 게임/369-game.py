e3784586ec6a174a3018c444ca028ccc38e45925
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-11-12 23:22:23 +0900

    Title: 369 게임, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231112/369 게임/369-game.py
+++ 231112/369 게임/369-game.py
@@ -3,5 +3,7 @@ n = int(input())
 for i in range(1, n+1):
     if i % 3 == 0:
         print(0, end=' ')
+    elif (i / 10) % 3 == 0:
+        print(0, end=' ')
     else:
         print(i, end= ' ')
