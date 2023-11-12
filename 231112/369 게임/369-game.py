6ae27b73cdd0cd349f018c97526d4d547f0f30c5
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-11-12 23:25:15 +0900

    Title: 369 게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231112/369 게임/369-game.py
+++ 231112/369 게임/369-game.py
@@ -1,9 +1,8 @@
 n = int(input())
 
 for i in range(1, n+1):
-    if i % 3 == 0:
+    if i % 10 % 3 == 0:
         print(0, end=' ')
-    elif (i / 10) % 3 == 0:
-        print(0, end=' ')
+    elif i / 10 % 3 == 0:
     else:
         print(i, end= ' ')
