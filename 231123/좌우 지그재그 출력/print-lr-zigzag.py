575c1604a511239d13c67b7964bcc3125608820f
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-11-23 06:09:01 +0900

    Title: 좌우 지그재그 출력, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231123/좌우 지그재그 출력/print-lr-zigzag.py
@@ -0,0 +1,16 @@
+n = int(input())
+cnt = 1
+minus = -1
+
+for i in range(n):
+    for j in range(n):
+        if i % 2 == 0:
+            print(cnt, end=" ")
+            cnt += 1
+            
+        else:
+            print(cnt+n+minus, end=" ")
+            minus -= 1
+    if i % 2 == 1:
+        cnt += n
+    print()
