8bb42af11f0886d2af19fc2b2271e95cb324ac08
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-11-23 06:18:46 +0900

    Title: 열에 따라 다르게 출력, Time: 133ms, Memory: 26MB, Status: Passed - CodeTree

--- 231123/열에 따라 다르게 출력/output-differently-for-column.py
+++ 231123/열에 따라 다르게 출력/output-differently-for-column.py
@@ -1,12 +1,17 @@
 n = int(input())
 
-cnt = 1
+cnt = 0
 
 for i in range(n):
     for j in range(n):
         if i % 2 == 0:
-            print(cnt,end=" ")
             cnt += 1
-        else i % 2 == 1:
             print(cnt,end=" ")
+            
+        else:
             cnt += 2
+            print(cnt,end=" ")
+            
+            
+
+    print()
