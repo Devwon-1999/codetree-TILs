ab1f2e94c29527ed60ec7cdd9420a24d25103fac
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-11-26 00:11:17 +0900

    Title: 소수 구하기, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 231126/소수 구하기/get-prime.py
+++ 231126/소수 구하기/get-prime.py
@@ -5,8 +5,8 @@ for i in range(1, n+1):
     for j in range(1, i+1):
         if i % j == 0:
             cnt += 1
-        if cnt == 2:
-            result.append(i)
+    if cnt == 2:
+        result.append(i)
 
 for i in result:
     print(i, end=" ")
