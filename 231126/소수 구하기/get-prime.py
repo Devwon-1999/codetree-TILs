899d43cdf8adf88214d7c9e74bfc3c46c4367fdc
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-11-26 00:10:38 +0900

    Title: 소수 구하기, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231126/소수 구하기/get-prime.py
@@ -0,0 +1,12 @@
+n = int(input())
+result = list()
+for i in range(1, n+1):
+    cnt = 0
+    for j in range(1, i+1):
+        if i % j == 0:
+            cnt += 1
+        if cnt == 2:
+            result.append(i)
+
+for i in result:
+    print(i, end=" ")
