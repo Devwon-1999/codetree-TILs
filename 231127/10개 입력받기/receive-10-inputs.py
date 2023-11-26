b9e4b1a5792736ecc770e5683aac02e5223eeeb5
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-11-27 01:45:19 +0900

    Title: 10개 입력받기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231127/10개 입력받기/receive-10-inputs.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+new = list()
+for i in arr:
+    if i == 0:
+        break
+    else:
+        new.append(i)
+
+avg = sum(new) / len(new)
+
+print(sum(new), avg)
