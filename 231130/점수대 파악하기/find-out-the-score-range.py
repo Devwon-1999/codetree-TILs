9051cfa0793fc35fb86fffaa1e028b96d8e235eb
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-11-30 17:32:10 +0900

    Title: 점수대 파악하기, Time: 109ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231130/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,13 @@
+scoreList = list(map(int, input().split()))
+resultList = [0 for i in range(10)]
+score = 100
+for i in scoreList:
+    if i == 0:
+        break
+    resultList[(i // 10)-1] += 1
+    
+resultList.reverse()
+
+for i in resultList:
+    print(f"{score} - {i}")
+    score -= 10
