f006be393ab8b3b904b33f0ad39d07fa6155d938
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-11-30 17:21:25 +0900

    Title: 십의 자리 숫자, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 231130/십의 자리 숫자/number-of-tens-digit.py
+++ 231130/십의 자리 숫자/number-of-tens-digit.py
@@ -1,16 +1,12 @@
-inputNum = list(map(int, input().split()))
-resultList = list(0 for _ in range(len(inputNum)+1))
-answerList = list(0 for _ in range(9))
-index = 1
+arr = list(map(int, input().split()))
+count_arr = [0] * 10
 
-for i in inputNum:
-    tenNum = (i % 100) // 10
-    resultList.append(tenNum)
-
-for i in resultList:
-    if i != 0:
-        answerList[i - 1] += 1
-
-for i in answerList:
-    print(f"{index} - {i}")
-    index += 1
+for elem in arr:
+	if elem == 0:
+		break
+	if elem < 10:
+		continue
+	count_arr[elem // 10] += 1
+	
+for i in range(1, 10):
+	print(f"{i} - {count_arr[i]}")
