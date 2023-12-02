2e4fb2d1e180db300cb79f420a4fe5fec7d39b16
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-02 19:45:55 +0900

    Title: 나눗셈의 나머지, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231202/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,36 @@
+a, b = map(int,input().split())
+remainList = list()
+cntList = [0 for i in range(10)]
+add = 0
+while a > 1:
+    remain = a % b
+    remainList.append(remain)
+    a //= b
+
+for i in remainList:
+    if i == 0:
+        cntList[0] += 1
+    elif i == 1:
+        cntList[1] += 1
+    elif i == 2:
+        cntList[2] += 1
+    elif i == 3:
+        cntList[3] += 1
+    elif i == 4:
+        cntList[4] += 1
+    elif i == 5:
+        cntList[5] += 1
+    elif i == 6:
+        cntList[6] += 1
+    elif i == 7:
+        cntList[7] += 1
+    elif i == 8:
+        cntList[8] += 1
+    elif i == 9:
+        cntList[9] += 1
+
+for i in cntList:
+    if i == 0:
+        continue
+    add += i ** i
+print(add)
