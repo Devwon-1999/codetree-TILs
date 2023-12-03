d5b789e55c09d129889ef7bb2628cf0202282c1b
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-03 20:04:38 +0900

    Title: 연속부분수열일까, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231203/연속부분수열일까/contiguous-array-or-not.py
@@ -0,0 +1,20 @@
+a, b = map(int, input().split())
+
+alist = list(map(int, input().split()))
+blist = list(map(int, input().split()))
+
+for i in range(a):
+    boolean = True
+    for j in range(b):
+        if i + j >= a:
+            boolean = False
+            break
+        if alist[i+j] != blist[j]:
+            boolean = False
+            break
+    if boolean:
+        print("Yes")
+        break
+    else:
+        print("No")
+        break
