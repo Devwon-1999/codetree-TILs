03067d5c3593dd17445d06ace444d721ec8f935a
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-04 23:17:35 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 231204/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 231204/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -7,8 +7,7 @@ maxUnique = -1
 numSet = set()
 
 for i in numList:
-    if i not in numSet:
-        numSet.add(i)
+    if i not in numSet and numList.count(i) == 1:
         maxUnique = max(maxUnique,i)
 
 print(maxUnique if maxUnique != -1 else -1)
