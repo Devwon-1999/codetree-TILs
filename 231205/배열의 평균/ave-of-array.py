bef958cd25c038956254909819660048791f9d52
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-05 18:32:41 +0900

    Title: 배열의 평균, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231205/배열의 평균/ave-of-array.py
+++ 231205/배열의 평균/ave-of-array.py
@@ -7,7 +7,7 @@ for i in numList:
     sumNum = 0
     for j in range(4):
         sumNum += i[j]
-    print("%.1f" % (sumNum//4), end=" ")
+    print("%.1f" % (sumNum/4), end=" ")
 print()
 
 for i in range(4):
@@ -17,7 +17,7 @@ for i in range(4):
         sumNum += numList[index][i]
         
         index += 1
-    print("%.1f" % (sumNum//2), end=" ")
+    print("%.1f" % (sumNum/2), end=" ")
 print()
 
 
@@ -26,4 +26,4 @@ for i in numList:
     for j in i:
         sumNum += j
 
-print("%.1f" % (sumNum//8), end=" ")
+print("%.1f" % (sumNum/8), end=" ")
