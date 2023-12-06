6caee4630e24bd0f63b2e6daa0dc803d3d1f679b
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-06 23:12:26 +0900

    Title: 지그재그로 숫자 채우기, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 231206/지그재그로 숫자 채우기/zigzag-numbering.py
+++ 231206/지그재그로 숫자 채우기/zigzag-numbering.py
@@ -1,24 +1,22 @@
-n, m = map(int, input().split())
-
-arr = [
+n, m = tuple(map(int, input().split()))
+answer = [
     [0 for _ in range(m)]
     for _ in range(n)
 ]
+count = 0
 
-num = 0
 
-for i in range(m):
-    if i % 2 == 0:
-        for j in range(n):
-            arr[j][i] = num
-            num += 1
+for col in range(m):
+    if col % 2 == 0:
+        for row in range(n):
+            answer[row][col] = count
+            count += 1
     else:
-        num += n - 1
-        for j in range(n):
-            arr[j][i] = num
-            num -= 1
+        for row in range(n - 1, -1, -1):
+            answer[row][col] = count
+            count += 1
 
-for i in range(n):
-    for j in range(m):
-        print(arr[i][j], end = " ")
+for row in range(n):
+    for col in range(m):
+        print(answer[row][col], end = ' ')
     print()
