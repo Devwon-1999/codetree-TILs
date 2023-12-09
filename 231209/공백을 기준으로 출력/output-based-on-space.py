88041bc8927e6ada9f8e3ff23c288aec5c13384c
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-09 22:35:55 +0900

    Title: 공백을 기준으로 출력, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231209/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,13 @@
+a = input()
+b = input()
+
+for i in a:
+    if i == " ":
+        continue
+    else:
+        print(i, end="")
+for i in b:
+    if i == " ":
+        continue
+    else:
+        print(i, end="")
