1fdd7ddb7a1c1575a1872b8da422098cde79c589
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-11 04:47:11 +0900

    Title: 특정 문자의 등장 횟수, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 231211/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 231211/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -2,10 +2,14 @@ s = input()
 eecnt = 0
 ebcnt = 0
 
-if "ee" in s:
-    eecnt += 1
+for i in range(len(s) - 1):
+    if s[i] == 'e' and s[i + 1] == 'e':
+        eecnt += 1   
 
-if "eb" in s:
-    ebcnt += 1
+for i in range(len(s) - 1):
+    if s[i] == 'e' and s[i + 1] == 'b':
+        ebcnt += 1
+
+    
 
 print(eecnt, ebcnt)
