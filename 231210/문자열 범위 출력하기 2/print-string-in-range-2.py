d8a601bc52354a2efacaa8e6a6dab6d0d03ab0b2
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-10 20:19:23 +0900

    Title: 문자열 범위 출력하기 2, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231210/문자열 범위 출력하기 2/print-string-in-range-2.py
+++ 231210/문자열 범위 출력하기 2/print-string-in-range-2.py
@@ -3,7 +3,8 @@ a = input()
 n = int(input())
 
 if len(a) < n:
-    print(a)
+    for i in a:
+        print(i,end="")
 else:
     for i in range(len(a)-1, len(a)-n-1, -1):
         print(a[i], end="")
