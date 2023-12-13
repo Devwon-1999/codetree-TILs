f93b1116f2673d7ea69f36fbfcaf064ce9261744
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-14 00:37:54 +0900

    Title: Run Length 인코딩, Time: 224ms, Memory: 24MB, Status: Passed - CodeTree

--- 231214/Run Length 인코딩/run-length-encoding.py
+++ 231214/Run Length 인코딩/run-length-encoding.py
@@ -1,20 +1,19 @@
-inputString = input()
-result = ""
+input_str = input()
+
+result = []  
 count = 1
 
-for i in range(1, len(inputString)):
-    if inputString[i] == inputString[i-1]:
+
+for i in range(1, len(input_str)):
+    if input_str[i] == input_str[i - 1]:
         count += 1
     else:
-        result += inputString[i-1] + str(count)
+        result.append(input_str[i - 1] + str(count))
         count = 1
-    if i == len(inputString) - 1:
-        result += inputString[i] + str(count)
 
-if inputString:
-    result += inputString[-1] + str(count)
 
-encoded_length = len(result)
+result.append(input_str[-1] + str(count))
 
-print(encoded_length)
-print(result)
+
+print(len("".join(result)))
+print("".join(result))
