8b12cf0eea5e7b6c4b95815c88c4e31bd946332d
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-14 00:14:18 +0900

    Title: Run Length 인코딩, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231214/Run Length 인코딩/run-length-encoding.py
@@ -0,0 +1,15 @@
+inputString = input()
+result = ""
+count = 1
+
+for i in range(1, len(inputString)):
+    if inputString[i] == inputString[i-1]:
+        count += 1
+    else:
+        result += inputString[i-1] + str(count)
+        count = 1
+    if i == len(inputString) - 1:
+        result += inputString[i] + str(count)
+
+print(len(result))
+print(result)
