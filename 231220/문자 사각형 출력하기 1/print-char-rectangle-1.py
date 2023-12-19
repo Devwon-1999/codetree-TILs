f4eb72bb930127bd3e7208ebc4856b2cc87b498f
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-20 06:46:09 +0900

    Title: 문자 사각형 출력하기 1, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231220/문자 사각형 출력하기 1/print-char-rectangle-1.py
+++ 231220/문자 사각형 출력하기 1/print-char-rectangle-1.py
@@ -8,7 +8,10 @@ for i in range(n):
     for j in range(n):
         if cnt > 90:
             cnt = 65
+        elif cnt < 65:
+            cnt = 65
         print(chr(cnt), end=" ")
+        
         cnt = cnt - n
     print()
     cnt = first - (i + 1)
