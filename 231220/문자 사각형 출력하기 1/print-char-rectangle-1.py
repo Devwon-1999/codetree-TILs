15a5319aae26587a5cb322e98505f381265d7b30
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-20 07:13:31 +0900

    Title: 문자 사각형 출력하기 1, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231220/문자 사각형 출력하기 1/print-char-rectangle-1.py
+++ 231220/문자 사각형 출력하기 1/print-char-rectangle-1.py
@@ -1,8 +1,10 @@
 n = int(input())
-cnt = 65 + (n * n) - 1
-if cnt > 90:
-    temp = cnt - 90
-    cnt = temp + 65 - 1
+cnt = 64
+for i in range(n * n):
+    cnt += 1
+    if cnt > 90:
+        cnt = 65
+
 first = cnt
 for i in range(n):
     for j in range(n):
