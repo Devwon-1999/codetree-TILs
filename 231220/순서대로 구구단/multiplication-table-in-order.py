af8febe797f20d9b5ab1c28c19bd7f2658a4969a
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-20 10:39:31 +0900

    Title: 순서대로 구구단, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231220/순서대로 구구단/multiplication-table-in-order.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+
+if a > b:
+    for i in range(1, 10):
+        for j in range(a, b - 1, -1):
+            print(f"{j} * {i} = {i * j}", end="  ")
+        print()
+
+else:
+    for i in range(1, 10):
+        for j in range(a, b + 1):
+            print(f"{j} * {i} = {i * j}", end="  ")
+        print()
