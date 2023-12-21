2bea7ffdb8a82516b8f2579efe980d2985b2c3ce
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-21 12:08:35 +0900

    Title: 숫자 출력하기 4, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231221/숫자 출력하기 4/print-number-rectangle-4.py
+++ 231221/숫자 출력하기 4/print-number-rectangle-4.py
@@ -7,10 +7,10 @@ for i in range(1, n+1):
     elif k == 2:
         if i % 2 == 1:
             for j in range(1, n+1):
-                print(i, end=" ")
+                print(j, end=" ")
         elif i % 2 == 0:
             for j in range(n, i - 1, -1):
-                print(i, end=" ")
+                print(j, end=" ")
         print()
 
     elif k == 3:
