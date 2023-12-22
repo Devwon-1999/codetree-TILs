1af0333155c01c257a50151d61d434045036eb1f
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-22 12:22:04 +0900

    Title: 공약수, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231222/공약수/common-divisor.py
@@ -0,0 +1,21 @@
+n = int(input())
+result = []
+if n == 2:
+    a, b = map(int, input().split())
+    if a > b:
+        for i in range(1, b+1):
+            if a % i == 0 and b % i == 0:
+                result.append(i)
+
+    elif a < b:
+        for i in range(1, a+1):
+            if a % i == 0 and b % i == 0:
+                result.append(i)
+
+elif n == 3:
+    numList = list(map(int, input().split()))
+    for i in range(1, min(numList) + 1):
+        if numList[0] % i == 0 and numList[1] % i == 0 and numList[2] % i == 0:
+            result.append(i)
+for i in result:
+    print(i)
