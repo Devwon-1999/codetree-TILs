bd89d69a6d19e191b309e66c02f927a339bbdb92
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-22 12:46:31 +0900

    Title: 100을 넘기는 순간, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 231222/100을 넘기는 순간/as-soon-as-hit-100.py
+++ 231222/100을 넘기는 순간/as-soon-as-hit-100.py
@@ -3,10 +3,7 @@ n = int(input())
 numList = list(map(int, input().split()))
 add = 0
 for i in numList:
-    if i < 100:
         add += i
-    elif i > 100:
-        add += i
-        
+
 print(add)
 print("%.1f" % (add/n))
