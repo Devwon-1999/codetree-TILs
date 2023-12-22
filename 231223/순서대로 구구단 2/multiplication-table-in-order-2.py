c03705538c3697f8f2bfda2f1a9efe17ca27bf3a
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-23 01:25:54 +0900

    Title: 순서대로 구구단 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231223/순서대로 구구단 2/multiplication-table-in-order-2.py
+++ 231223/순서대로 구구단 2/multiplication-table-in-order-2.py
@@ -1,8 +1,18 @@
 a, b = map(int, input().split())
 
 for i in range(3 * (a - b)):
+    
+    
     for j in range(1, 10):
-        print(f"{a} * {j} = {a * j}", end=" ")
+        print(f"{a} * {j} = {a * j}", end="  ")
         if j % 3 == 0:
             print()
     print()
+    if a > b:
+        a -= 1
+        if a < b:
+            break    
+    else:
+        a += 1
+        if a > b:
+            break
