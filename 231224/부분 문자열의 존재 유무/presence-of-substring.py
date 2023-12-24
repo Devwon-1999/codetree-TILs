ae39490c6f921370b6637a15ee143dbe582ae9ff
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-24 11:09:40 +0900

    Title: 부분 문자열의 존재 유무, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 231224/부분 문자열의 존재 유무/presence-of-substring.py
+++ 231224/부분 문자열의 존재 유무/presence-of-substring.py
@@ -2,13 +2,11 @@ a = input()
 
 eeTrue = "NO"
 eaTrue = "NO"
-for i in range(len(a)):
-    if i + 1 > len(a):
-        break
-    if a[i] == "e" and a[i+1] == "e":
-        eeTrue = "YES"
-    if a[i] == "e" and a[i+1] == "a":
-        eaTrue = "YES"
+
+if "ee" in a:
+    eeTrue = "YES"
+if "ea" in a:
+    eaTrue = "YES"
     
 
 print(eeTrue, eaTrue)
