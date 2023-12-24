a32e6068da8899cd9f34ac92ba8566c32f5ee05f
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-24 10:59:41 +0900

    Title: 부분 문자열의 존재 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231224/부분 문자열의 존재 유무/presence-of-substring.py
@@ -0,0 +1,12 @@
+a = input()
+index = 1
+eeTrue = "NO"
+eaTrue = "NO"
+for i in a:
+    if i == "e" and a[index] == "e":
+        eeTrue = "YES"
+    if i == "e" and a[index] == "a":
+        eaTrue = "YES"
+    index += 1
+
+print(eeTrue, eaTrue)
