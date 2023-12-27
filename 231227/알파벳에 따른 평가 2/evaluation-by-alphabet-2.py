039d30d032548a55d3482c843dda6c68032218e4
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-27 16:32:25 +0900

    Title: 알파벳에 따른 평가 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231227/알파벳에 따른 평가 2/evaluation-by-alphabet-2.py
@@ -0,0 +1,12 @@
+a = input()
+
+if a == "S":
+    print("Superior")
+elif a == "F":
+    print("Fantastic")
+elif a == "G":
+    print("Good")
+elif a == "U":
+    print("Usually")
+elif a == "S":
+    print("")
