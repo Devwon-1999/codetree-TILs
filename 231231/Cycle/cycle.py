6c7ac36cdd0faf0c9a562e093dc5d07e3428a5a5
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2023-12-31 09:47:37 +0900

    Title: Cycle, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 231231/Cycle/cycle.py
+++ 231231/Cycle/cycle.py
@@ -1,12 +1,13 @@
 N, P = map(int, input().split())
-result = []
+
+seen = []
+current = N
 cnt = 0
-for i in range(100):
-    N = (N * N) % P
-    result.append(N)
-    
-for i in range(1, 100):
+
+while current not in seen:
+    seen.append(current)
+    current = (current * N) % P
     cnt += 1
-    if result[0] == result[i]:
-        print(cnt + 1)
-        break
+
+result = cnt - seen.index(current)
+print(result)
