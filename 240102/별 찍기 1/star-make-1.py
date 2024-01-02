18869850ca40b31575a0a4c43af242de68e87b5b
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-02 14:07:37 +0900

    Title: 별 찍기 1, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240102/별 찍기 1/star-make-1.py
@@ -0,0 +1,20 @@
+size, order = map(int, input().split())
+temp = 1
+if order == 1:
+    for i in range(1, size + 1):
+        for j in range(i):
+            print("*", end="")
+        print()
+elif order == 2:
+    for i in range(size, 0, -1):
+        for j in range(i):
+            print("*", end="")
+        print()            
+elif order == 3:
+    for i in range(size, 0, -1):
+        for j in range(i):
+            print("", end = " ")
+        for j in range(temp):
+            print("*", end = "")
+        temp += 2
+        print()
