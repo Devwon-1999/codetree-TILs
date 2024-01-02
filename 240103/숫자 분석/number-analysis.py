62fadaac602420049bb3f42a0e7c30e3dee8f368
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-03 08:33:11 +0900

    Title: 숫자 분석, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240103/숫자 분석/number-analysis.py
+++ 240103/숫자 분석/number-analysis.py
@@ -1,11 +1,11 @@
-n = list(map(int, input()))
+user_input = input().strip()
+n = list(map(int, user_input))
+
 total = 0
 
 for i in reversed(n):
     print(i, end="")
 print(" ", end="")
-
 for i in n:
     total += i
-
 print(total)
