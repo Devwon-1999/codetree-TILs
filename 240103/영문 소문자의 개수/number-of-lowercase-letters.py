bf6ac10272f8be695317272eac15b7a096be4f6b
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-03 09:12:21 +0900

    Title: 영문 소문자의 개수, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240103/영문 소문자의 개수/number-of-lowercase-letters.py
+++ 240103/영문 소문자의 개수/number-of-lowercase-letters.py
@@ -2,12 +2,19 @@ n = int(input())
 abcList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 strList = list(input().split())
 resultList = []
+tempList = []
 
 for i in strList:
     if i in abcList:
         resultList.append(i)
     else:
         break
+
 resultList.sort()
+
 for i in resultList:
+    if i not in tempList:
+        tempList.append(i)
+
+for i in tempList:
     print(f"{i} : {resultList.count(i)}")
