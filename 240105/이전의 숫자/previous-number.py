89c552700d1f091770b1341a30a59236ea1d8bd2
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-05 10:47:27 +0900

    Title: 이전의 숫자, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240105/이전의 숫자/previous-number.py
+++ 240105/이전의 숫자/previous-number.py
@@ -1,9 +1,10 @@
 n = int(input())
 numList = list(map(int, input().split()))
 
+indexZero = numList.index(0)
 result = 0
 
-for i in numList[-6:-1]:
+for i in numList[indexZero - 5:indexZero]:
     result += i
 
 print(result)
