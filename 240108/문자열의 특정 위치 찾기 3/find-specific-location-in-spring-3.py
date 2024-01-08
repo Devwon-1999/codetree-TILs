95fd006923dc65887af6899ae282dfdc0d37143a
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-08 11:11:02 +0900

    Title: 문자열의 특정 위치 찾기 3, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240108/문자열의 특정 위치 찾기 3/find-specific-location-in-spring-3.py
+++ 240108/문자열의 특정 위치 찾기 3/find-specific-location-in-spring-3.py
@@ -1,7 +1,9 @@
 baseStr = input()
 search = input()
 
-if baseStr.find(search):
-    print(baseStr.find(search) + 1)
+
 if baseStr.find(search) == -1:
     print('Not Found')
+
+elif baseStr.find(search):
+    print(baseStr.find(search) + 1)
