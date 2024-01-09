a0abda862caba93051b3b386994bdbeb1445842c
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-09 12:04:15 +0900

    Title: 문자 삼각형 출력하기 1, Time: 157ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240109/문자 삼각형 출력하기 1/print-char-triangle-1.py
@@ -0,0 +1,14 @@
+n = int(input())
+cnt = 65
+for i in range(1, n+1):
+    for j in range(n - i):
+        print(" ", end= " ")
+        
+    for j in range(1, i+1):
+        if cnt == 91:
+            cnt = 65
+        print(chr(cnt), end= " ")    
+        cnt = cnt + (n - j)
+        
+    cnt = 65 + i
+    print()
