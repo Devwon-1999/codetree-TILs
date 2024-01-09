b070f03b63dc0089b6f6aee1d9d1b4efcb2a2027
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-09 12:24:39 +0900

    Title: 숫자 찍기 1, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240109/숫자 찍기 1/num-make-1.py
@@ -0,0 +1,34 @@
+size, order = map(int, input().split())
+
+if order == 1:
+    num = 1
+    for i in range(1, size + 1):
+        if i % 2 == 0:
+            for j in range(num + i - 1, num - 1, -1):
+                print(j, end=" ")
+            num += i
+        else:
+            for j in range(num, num + i):
+                print(j, end=" ")
+            num += i
+        print()
+
+elif order == 2:
+    num = 0
+    for i in range(5, 0, -1):
+        for j in range(size - i):
+            print(" ", end = " ")
+        for j in range(i * 2 - 1):
+            print(num, end = " ")
+        print()
+        num += 1
+
+elif order == 3:
+    for i in range(1, size + 1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
+    for i in range(size - 1, 0, -1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
