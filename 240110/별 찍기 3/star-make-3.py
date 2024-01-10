3273dab9e05dbecef0bffe0bbacfded72d87f318
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-10 19:02:55 +0900

    Title: 별 찍기 3, Time: 152ms, Memory: 26MB, Status: Passed - CodeTree

--- 240110/별 찍기 3/star-make-3.py
+++ 240110/별 찍기 3/star-make-3.py
@@ -1,12 +1,14 @@
 n = int(input())
-
+cnt = n
+start = 1
 for i in range(n // 2 + 1):
     for j in range(i):
         print(" ", end ="")
     
-    for j in range(1, i + 2):
-        print("*" * j, end = "")
+    for j in range(start):
+        print("*" , end = "")
     print()
+    start += 2
 cnt = n
 
 for i in range(n // 2, 0, -1):
