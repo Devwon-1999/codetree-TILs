e4949b75a4e1b166e65c2f5a59b2576c8124cf08
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-11 19:55:54 +0900

    Title: 진법 변환 6, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240111/진법 변환 6/base-conversion-6.py
+++ 240111/진법 변환 6/base-conversion-6.py
@@ -1,19 +1,18 @@
-base, num, trans = map(int, input().split()) # 입력
-
-
-if base != 10:              
-    num = str(num)         
-    num_10 = int(num, base) 
-
+base, num, trans = map(int, input().split())  
+if base != 10:
+    num = str(num)
+    num_10 = int(num, base)
 
 def solution(n, q):
     rev_base = ''
 
     while n > 0:
         n, mod = divmod(n, q)
-        rev_base += str(mod)
+        if mod >= 10:
+            rev_base += chr(ord('A') + mod - 10)
+        else:
+            rev_base += str(mod)
 
-    return rev_base[::-1] 
-    
-    
+    return rev_base[::-1]
+
 print(solution(num_10, trans))
