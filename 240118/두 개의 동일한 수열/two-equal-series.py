42c5c6032c0c62d38720a7231109a9e7d4bccdbc
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-18 20:51:12 +0900

    Title: 두 개의 동일한 수열, Time: 150ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240118/두 개의 동일한 수열/two-equal-series.py
+++ 240118/두 개의 동일한 수열/two-equal-series.py
@@ -5,11 +5,9 @@ list2 = list(map(int, input().split()))
 list1.sort()
 list2.sort()
 
-if len(list1) == len(list2):
-    for i in range(len(list1)):
-        if list1[i] != list2[i]:
-            print("No")
-            break
-    print("Yes")
-else:
-    print("No")
+
+for i in range(len(list1)):
+    if list1[i] != list2[i]:
+        print("No")
+        break
+print("Yes")
