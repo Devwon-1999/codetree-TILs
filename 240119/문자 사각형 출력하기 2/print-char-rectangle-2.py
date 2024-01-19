6e619d43507fa29eea44e43c081e1086ed6a72b5
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-19 18:55:05 +0900

    Title: 문자 사각형 출력하기 2, Time: 128ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240119/문자 사각형 출력하기 2/print-char-rectangle-2.py
+++ 240119/문자 사각형 출력하기 2/print-char-rectangle-2.py
@@ -14,11 +14,13 @@ for i in range(n):
             a += trans1
             if a > 90:
                 a = (a % 90) % 25 + 65 
+                a -= 1
                                                                                                                                                     
         else:
             a += trans2
             if a > 90:
                 a = (a % 90) % 25 + 65 
+                a -= 1
 
     trans1 -= 2
     trans2 += 2
