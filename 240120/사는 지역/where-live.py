3f2dbe119cda24ff200bf4185db81c51127500b3
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-20 17:01:07 +0900

    Title: 사는 지역, Time: 161ms, Memory: 24MB, Status: Passed - CodeTree

--- 240120/사는 지역/where-live.py
+++ 240120/사는 지역/where-live.py
@@ -7,6 +7,7 @@ class address:
 n = int(input())
 
 nameList = []
+people = []
 for i in range(n):
     na, ad, pl = input().split()
 
@@ -14,10 +15,27 @@ for i in range(n):
 
     nameList.sort()
 
-    na = address(na, ad, pl)
+    people.append(address(na, ad, pl))
+
+for i in range(n):
+    if people[i].name == nameList[-1]:
+        print(f"name {people[i].name}")
+        print(f"addr {people[i].address}")
+        print(f"city {people[i].place}")
 
 
-if na.name == nameList[-1]:
-    print(f"name {na.name}")
-    print(f"addr {na.address}")
-    print(f"city {na.place}")
+
+
+
+# agents_score = []
+# for i in range(5):
+#     agents_score.append(agents[i].score)
+
+# min_S = min(agents_score)
+# min_N = ""
+# for i in range(5):
+#     if agents[i].score == min_S:
+#         min_N = agents[i].name
+
+
+# print(min_N, min_S)
