91368dd1f5762421db0921e003ff8111f5824843
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-21 22:45:16 +0900

    Title: 요일 맞추기, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240121/요일 맞추기/guess-day-of-week.py
+++ 240121/요일 맞추기/guess-day-of-week.py
@@ -1,12 +1,7 @@
-def day_of_week(year, month, day):
+def day_of_week(month, day):
     days_of_week = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"]
-    elapsed_days = 0
-    for y in range(2011, year):
-        if y % 4 == 0 and (y % 100 != 0 or y % 400 == 0):
-            elapsed_days += 366
-        else:
-            elapsed_days += 365
 
+    elapsed_days = 0
     for m in range(1, month):
         elapsed_days += num_of_days[m]
 
@@ -14,13 +9,16 @@ def day_of_week(year, month, day):
 
     return days_of_week[(elapsed_days + 5) % 7]
 
+
 m1, d1, m2, d2 = map(int, input().split())
 
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-if (m1 == m2 and d1 <= d2) or (m1 < m2):
-    result = day_of_week(2011, m2, d2)
-    print(result)
+if m1 == m2 and d1 == d2:
+    result = day_of_week(m2, d2)
 else:
-    result = day_of_week(2011, m2, d2)
-    print(result)
+    if (m1 == m2 and d1 <= d2) or (m1 < m2):
+        result = day_of_week(m2, d2)
+    else:
+        result = day_of_week(m2, d2)
+print(result)
