dca6cb5bd540927c6a78b72c5a445830a608a34c
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-21 19:42:03 +0900

    Title: 수업태도 평가, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240121/수업태도 평가/class-attitude-assessment.py
@@ -0,0 +1,37 @@
+class class_attitude():
+    def __init__(self, name, score=0):
+        self.name = name
+        self.score = score
+
+n = int(input())
+
+students = []
+
+for i in range(n):
+    name, score = input().split()
+    score = int(score)
+    
+    found = False
+    for student in students:
+        if student.name == name:
+            student.score += score
+            found = True
+            break
+    
+    if not found:
+        students.append(class_attitude(name, score))
+        
+students_sorted = sorted(students, key=lambda x: x.score, reverse=True)
+lowest_score = students_sorted[-1].score
+second_lowest_score = None
+for student in reversed(students_sorted):
+    if student.score > lowest_score:
+        second_lowest_score = student.score
+        break
+
+second_lowest_students = [student.name for student in students_sorted if student.score == second_lowest_score]
+
+if len(second_lowest_students) == 1:
+    print(second_lowest_students[0])
+else:
+    print("Tie")
