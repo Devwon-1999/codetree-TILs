64bddc7dfaaf99e021d63b910fa2f7079e53a900
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-21 19:47:22 +0900

    Title: 수업태도 평가, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240121/수업태도 평가/class-attitude-assessment.py
+++ 240121/수업태도 평가/class-attitude-assessment.py
@@ -4,34 +4,38 @@ class class_attitude():
         self.score = score
 
 n = int(input())
-
 students = []
-
-for i in range(n):
+if n == 1:
     name, score = input().split()
-    score = int(score)
-    
-    found = False
-    for student in students:
-        if student.name == name:
-            student.score += score
-            found = True
-            break
-    
-    if not found:
-        students.append(class_attitude(name, score))
-        
-students_sorted = sorted(students, key=lambda x: x.score, reverse=True)
-lowest_score = students_sorted[-1].score
-second_lowest_score = None
-for student in reversed(students_sorted):
-    if student.score > lowest_score:
-        second_lowest_score = student.score
-        break
-
-second_lowest_students = [student.name for student in students_sorted if student.score == second_lowest_score]
-
-if len(second_lowest_students) == 1:
-    print(second_lowest_students[0])
+    print(name)
 else:
-    print("Tie")
+    for i in range(n):
+        name, score = input().split()
+        score = int(score)
+        
+        found = False
+        for student in students:
+            if student.name == name:
+                student.score += score
+                found = True
+                break
+        
+        if not found:
+            students.append(class_attitude(name, score))
+
+
+    students_sorted = sorted(students, key=lambda x: x.score, reverse=True)
+    lowest_score = students_sorted[-1].score
+    second_lowest_score = None
+
+    for student in reversed(students_sorted):
+        if student.score > lowest_score:
+            second_lowest_score = student.score
+            break
+
+    second_lowest_students = [student.name for student in students_sorted if student.score == second_lowest_score]
+
+    if len(second_lowest_students) == 1:
+        print(second_lowest_students[0])
+    else:
+        print("Tie")
