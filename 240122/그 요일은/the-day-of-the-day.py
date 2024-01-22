645def60051660ed312278e45d1db67e8698dc55
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-22 23:33:14 +0900

    Title: 그 요일은, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240122/그 요일은/the-day-of-the-day.py
+++ 240122/그 요일은/the-day-of-the-day.py
@@ -1,6 +1,7 @@
 def day_of_week(year, month, day):
     days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
+
     elapsed_days = 0
     for y in range(2024, year):
         if y % 4 == 0 and (y % 100 != 0 or y % 400 == 0):
@@ -13,17 +14,21 @@ def day_of_week(year, month, day):
 
     elapsed_days += day - 1
 
+
     return days_of_week[elapsed_days % 7]
 
 
 m1, d1, m2, d2 = map(int, input().split())
 target_day = input().strip()
 
+
 num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
 count = 0
 current_day = day_of_week(2024, m1, d1)
 
-while not (m1 == m2 and d1 == d2):
+while not (m1 == m2 and d1 == d2 + 1):
     if current_day == target_day:
         count += 1
 
@@ -34,7 +39,5 @@ while not (m1 == m2 and d1 == d2):
 
     current_day = day_of_week(2024, m1, d1)
 
-if current_day == target_day:
-    count += 1
 
 print(count)
