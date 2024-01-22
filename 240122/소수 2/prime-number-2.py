8cccbf5ba050ee66a857f4bc7356d0cb1decb8b8
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-22 22:24:24 +0900

    Title: 소수 2, Time: 200ms, Memory: 25MB, Status: Passed - CodeTree

--- 240122/소수 2/prime-number-2.py
+++ 240122/소수 2/prime-number-2.py
@@ -1,13 +1,15 @@
+def count_primes(n):
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+
+    primes = [num for num in range(2, n + 1) if is_prime[num]]
+    return len(primes)
+
 n = int(input())
-result = []
-
-for i in range(2, n + 1):
-    temp = []
-    for j in range(1, i + 1):
-        if i % j == 0:
-            temp.append(j)
-    if len(temp) == 2:
-        result.append(i)
-
-
-print(len(result))
+result = count_primes(n)
+print(result)
