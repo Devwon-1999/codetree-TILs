a18643e225a22952d150b3c0422bdc797e97b198
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-26 09:59:25 +0900

    Title: 두 수의 최대공약수와 최소공배수, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240126/두 수의 최대공약수와 최소공배수/common-divisor-multiple-1.py
@@ -0,0 +1,13 @@
+def lcm(a, b): #최대공약수
+    return a * b // gcd(a, b) 
+
+
+def gcd(a, b):  #최소공배수
+    while b:
+        a, b = b, a % b
+    return a
+
+
+a, b = map(int, input().split())
+
+print(gcd(a, b), lcm(a, b))
