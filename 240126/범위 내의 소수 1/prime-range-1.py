05c1c55482df66c3167ecff34bf91d3b5af6348e
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-26 09:44:23 +0900

    Title: 범위 내의 소수 1, Time: 640ms, Memory: 28MB, Status: Passed - CodeTree

--- 240126/범위 내의 소수 1/prime-range-1.py
+++ 240126/범위 내의 소수 1/prime-range-1.py
@@ -1,12 +1,10 @@
 def isPrime(n):
-    cnt = 0
-    for i in range(1, n + 1):
-        if n % i == 0:
-            cnt += 1
-    if cnt == 2:
-        return True
-    else:
+    if n < 2:
         return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 
 a, b = map(int, input().split())
