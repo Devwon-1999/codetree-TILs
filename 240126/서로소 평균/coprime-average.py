531dab050c481173f373b32dada95a63f9742c02
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-26 10:13:27 +0900

    Title: 서로소 평균, Time: 209ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240126/서로소 평균/coprime-average.py
@@ -0,0 +1,15 @@
+def gcd(a, b):  #최소공배수
+    while b:
+        a, b = b, a % b
+    return a
+
+n = int(input())
+numlist = list(map(int, input().split()))
+x = int(input())
+result = []
+
+for i in numlist:
+    if gcd(x, i) == 1:
+        result.append(i)
+
+print("%.2f" % (sum(result) / len(result)))
