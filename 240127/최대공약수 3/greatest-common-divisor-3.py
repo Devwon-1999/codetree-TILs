64bdb995abcb73d45cbc3b5e046458cd0f47fdbd
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-27 17:16:09 +0900

    Title: 최대공약수 3, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240127/최대공약수 3/greatest-common-divisor-3.py
@@ -0,0 +1,21 @@
+def gcd(a, b): #최대공약수
+    while b:
+        a, b = b, a % b
+    return a
+
+
+n = int(input())
+n_list = list(map(int, input().split()))
+
+m = int(input())
+m_list = list(map(int, input().split()))
+
+a = 1
+for i in n_list:
+    a *= i
+
+b = 1
+for i in m_list:
+    b *= i
+
+print(gcd(a, b))
