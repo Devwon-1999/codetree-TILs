6e6d482a1ffb9e75e0a2be7a929f0efd7d9349fc
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-28 20:39:01 +0900

    Title: 대칭수와 소수, Time: 168ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240128/대칭수와 소수/symmetrical-numbers-and-decimals.py
@@ -0,0 +1,25 @@
+def isPrime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def palindromic(n):
+    n = str(n)
+    rev_n = "".join(reversed(n))
+    if n == rev_n:
+        return True
+    else:
+        return False
+
+n = int(input())
+
+for i in range(n, 1000000):
+    if isPrime(i) and palindromic(i):
+        print(i)
+        break
+    else:
+        continue
