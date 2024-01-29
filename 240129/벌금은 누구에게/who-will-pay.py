053c6af5e089c9a6c1646c703662074e3b455c56
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-01-29 22:22:21 +0900

    Title: 벌금은 누구에게, Time: 144ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240129/벌금은 누구에게/who-will-pay.py
+++ 240129/벌금은 누구에게/who-will-pay.py
@@ -1,10 +1,18 @@
 N, M, K = map(int, input().split())
 
 result = [0 for i in range(N)]
-
+answer = []
 for i in range(M):
     student = int(input())
 
-    result[student-1] += 1
+    result[student - 1] += 1
 
-print(result.index(max(result)) + 1)
+for i in result:
+    if i >= K:
+        answer.append(i)
+        print(result.index(max(result)) + 1)
+
+if answer:
+    pass
+else:
+    print(-1)
