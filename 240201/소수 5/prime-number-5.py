99ec02f7b26eee63d9192f51ff281b1768deb5b7
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-01 16:26:49 +0900

    Title: 소수 5, Time: 5031ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

--- 240201/소수 5/prime-number-5.py
+++ 240201/소수 5/prime-number-5.py
@@ -1,20 +1,14 @@
-def isPrime(n): #소수 여부 확인
-    if n < 2:
+def is_prime(num):
+    if num < 2:
         return False
-    
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
             return False
-
     return True
 
+def count_primes(a, b):
+    return sum(1 for num in range(a, b + 1) if is_prime(num))
+
 a, b = map(int, input().split())
-result = 0
-
-for i in range(a, b + 1):
-    if isPrime(i):
-        result += 1
-    else:
-        continue
-
+result = count_primes(a, b)
 print(result)
