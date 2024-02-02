7aa64f7def1a168de6033a520e616c62b7d251b1
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-02 17:41:56 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240202/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,33 @@
+n = int(input())
+now = 10
+result = [" " for i in range(21)]
+
+for i in range(n):
+	size, order = input().split()
+	size = int(size)	
+
+	if order == "L": #왼쪽으로
+		for i in range(now, now - size, -1):
+			if result[i] == " " or result[i] == "black":
+				result[i] = "white"
+		now -= size - 1
+
+	elif order == "R": #오른쪽으로
+		for i in range(now, now + size):
+			if result[i] == " " or result[i] == "white":
+				result[i] = "black"
+		now += size - 1
+
+
+black_cnt = 0
+white_cnt = 0
+for i in result:
+	if i == " ":
+		continue
+	elif i == "black":
+		black_cnt += 1
+	elif i == "white":
+		white_cnt += 1
+
+
+print(white_cnt, black_cnt)
