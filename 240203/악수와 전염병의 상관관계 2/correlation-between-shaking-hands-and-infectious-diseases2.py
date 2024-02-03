a8c8dbd7210dc6d250629455cee92f07d952e715
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-03 15:17:12 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240203/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,36 @@
+N, K, P, T = map(int, input().split())
+#N 명의 개발자
+#K 번의 악수 동안만 전파
+#P 최초 감염자
+#T 반복
+developer = [-1 for i in range(N)]
+spread = [K for i in range(N)]
+
+developer[P - 1] = 1
+T_List = []
+
+for i in range(T):
+    t, x, y = map(int, input().split()) # t초에 x와 y가 악수를 함.
+    
+    T_List.append([t, x, y])
+
+T_List.sort()
+
+for i in T_List:
+    if (i[1] == P or developer[i[1] - 1] == 1) and spread[i[1] - 1] > 0: # 악수 리스트중 [1] 이 양성이고 전파력이 있을때 전파
+        developer[i[2] - 1] = 1
+        spread[i[1] - 1] -= 1
+        
+    elif (i[2] == P or developer[i[2] - 1] == 1) and spread[i[2] - 1] > 0: # 악수 리스트중 [2] 이 양성이고 전파력이 있을때 전파
+        developer[i[1] - 1] = 1
+        spread[i[2] - 1] -= 1
+
+result = ""
+
+for i in developer:
+    if i == -1:
+        result += "0"
+    elif i == 1:
+        result += "1"
+
+print(result)
