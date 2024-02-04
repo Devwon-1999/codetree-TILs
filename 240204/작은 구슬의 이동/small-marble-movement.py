582d49d97419011d48555e7bb936c0c5d9611e06
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-04 21:48:40 +0900

    Title: 작은 구슬의 이동, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240204/작은 구슬의 이동/small-marble-movement.py
+++ 240204/작은 구슬의 이동/small-marble-movement.py
@@ -1,11 +1,9 @@
 n, t = map(int, input().split()) # 격자 크기 n 움직이는 시간 t
 r, c, d = input().split() #최초 좌표 (r, c) 방향 d
-r, c = int(r), int(c) # 좌표의 정수화 r 행, c 열
-x = c - 1
-y = r - 1
+x, y = int(r) - 1, int(c) - 1 # 좌표의 정수화 r 행, c 열
+
 
 dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1]
-
 mapper = { 'R': 0, 'D': 1, 'U': 2, 'L': 3 }
 
 move_dir = mapper[d] #입력된 방향의 인덱스 대입
@@ -14,10 +12,10 @@ def in_range(x, y): # 격자 값 확인
 
 for i in range(t):
     nx, ny = x + dxs[move_dir], y + dys[move_dir]
-    if not in_range(nx, ny):
+    if in_range(nx, ny):
+        x, y = nx, ny
+    else:
         move_dir = 3 - move_dir
-        continue
+    
 
-    x, y = nx, ny
-
-print(x, y)
+print(x + 1, y + 1)
