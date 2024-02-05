2b8aea0f1030e0248f0bd5a91a73c3269fa19827
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-05 18:02:42 +0900

    Title: 되돌아오기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240205/되돌아오기 2/come-back-2.py
@@ -0,0 +1,29 @@
+x, y = 0, 0
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0] #북, 동, 남, 서
+dir_num = 0
+
+
+
+
+
+order = input()
+orderList = []
+for i in order:
+    orderList.append(i)
+
+result = []
+time = 0
+for i in orderList:
+    time += 1
+    if i == "L": #좌측 90
+        dir_num = (dir_num + 3) % 4
+    elif i == "R": #우측 90
+        dir_num = (dir_num + 1) % 4
+    elif i == "F": # 이동
+        x, y = x + dxs[dir_num], y + dys[dir_num]
+
+    if x == 0 and y == 0:
+        result.append(time)
+
+
+print(result[0])
