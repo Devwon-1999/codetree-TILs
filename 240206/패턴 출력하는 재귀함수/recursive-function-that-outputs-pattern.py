8610b2bebaffbe7f317f4844dada0ac8d6a8a2f2
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-06 21:57:46 +0900

    Title: 패턴 출력하는 재귀함수, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240206/패턴 출력하는 재귀함수/recursive-function-that-outputs-pattern.py
+++ 240206/패턴 출력하는 재귀함수/recursive-function-that-outputs-pattern.py
@@ -1,10 +1,10 @@
 def print_odd_numbers(n):
     if n <= 369:
-        if n != 369:
-            print_odd_numbers(n + 2)
+        
         if n % 2 != 0:
             print(n, end=" ")
-        
+        if n != 369:
+            print_odd_numbers(n + 2)
     else:
         if n != 369:
             print_odd_numbers(n - 2)
