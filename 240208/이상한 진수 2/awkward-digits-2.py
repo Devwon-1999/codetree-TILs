2c0a08d11b47460ebd07ec565fc225e5a94cc204
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-08 17:03:06 +0900

    Title: 이상한 진수 2, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240208/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,21 @@
+a = input()
+
+#입력된 문자열 형식의 a를 하나로 쪼갬
+a_list = [] 
+for i in a: 
+    i = int(i)
+    a_list.append(i)
+
+#제일 앞자리에 있는 0을 1로 변경
+for index, value in enumerate(a_list): 
+    if value == 0:
+        a_list[index] = 1
+        break
+
+#변경된 이진수 정수화
+num = 0
+for i in a_list:
+    i = int(i)
+    num = num * 2 + i
+
+print(num)
