00792c26b7d80b06f2af3b05f515fc03f015aef7
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-09 22:34:05 +0900

    Title: 정수 두 개의 크기 비교, Time: 5028ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240209/정수 두 개의 크기 비교/compare-the-size-of-two-integers.py
+++ 240209/정수 두 개의 크기 비교/compare-the-size-of-two-integers.py
@@ -1,6 +1,7 @@
 n = int(input())
 numList = list(map(int, input().split()))
 cnt = 0
+numList.sort()
 for i in range(n):
     for j in range(i + 1, n):
         if min(numList[i],numList[j]) >= max(numList[i],numList[j]) * 0.9:
