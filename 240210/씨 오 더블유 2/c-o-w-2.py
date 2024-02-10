a58222e51325249f1ff312ffca0078830f6f2fcc
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-10 12:18:41 +0900

    Title: 씨 오 더블유 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240210/씨 오 더블유 2/c-o-w-2.py
+++ 240210/씨 오 더블유 2/c-o-w-2.py
@@ -4,9 +4,11 @@ string = input()
 result = []
 
 for i in range(N):
-    for j in range(i, N):
-        for k in range(i, N):
-            if string[i] == "C" and string[j] == "O" and string[k] == "W":
-                result.append([i+1, j+1, k+1])
+    if string[i] == "C":
+        for j in range(i, N):
+            if string[j] == "O":
+                for k in range(i, N):
+                    if string[k] == "W":
+                        result.append([i+1, j+1, k+1])
 
 print(len(result))
