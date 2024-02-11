5136975bbf684499f5f76e7c9a8ccaf74773a221
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-11 22:38:57 +0900

    Title: 소수 5, Time: 689ms, Memory: 101MB, Status: Passed - CodeTree

--- 240211/소수 5/prime-number-5.py
+++ 240211/소수 5/prime-number-5.py
@@ -1,13 +1,16 @@
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n+1)
+    primes[0] = primes[1] = False
+    for i in range(2, int(n**0.5)+1):
+        if primes[i]:
+            for j in range(i*i, n+1, i):
+                primes[j] = False
+    return primes
 
 def count_primes(a, b):
-    return sum(1 for num in range(a, b + 1) if is_prime(num))
+    primes = sieve_of_eratosthenes(b)
+    count = sum(1 for i in range(a, b+1) if primes[i])
+    return count
 
 a, b = map(int, input().split())
 result = count_primes(a, b)
