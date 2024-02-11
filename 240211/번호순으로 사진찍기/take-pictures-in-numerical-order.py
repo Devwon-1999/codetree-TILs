82c469ac03c62bbfdc15225e7e9e28f02141b197
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-11 22:17:24 +0900

    Title: 번호순으로 사진찍기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240211/번호순으로 사진찍기/take-pictures-in-numerical-order.py
+++ 240211/번호순으로 사진찍기/take-pictures-in-numerical-order.py
@@ -1,10 +1,41 @@
+def find(parent, i):
+    if parent[i] == i:
+        return i
+    return find(parent, parent[i])
+
+def union(parent, rank, x, y):
+    x_root = find(parent, x)
+    y_root = find(parent, y)
+
+    if rank[x_root] < rank[y_root]:
+        parent[x_root] = y_root
+    elif rank[x_root] > rank[y_root]:
+        parent[y_root] = x_root
+    else:
+        parent[y_root] = x_root
+        rank[x_root] += 1
+
+def min_photos(N, dislikes):
+    parent = [i for i in range(N + 1)]
+    rank = [0] * (N + 1)
+
+    for dislike in dislikes:
+        a, b = dislike
+        union(parent, rank, a, b)
+
+    groups = set()
+    for i in range(1, N + 1):
+        groups.add(find(parent, i))
+
+    return len(groups)
+
+# 입력 받기
 N, K = map(int, input().split())
+dislikes = []
+for _ in range(K):
+    a, b = map(int, input().split())
+    dislikes.append((a, b))
 
-
-not_friendly = []
-
-for i in range(K):
-    temp = list(map(int, input().split()))
-    not_friendly.append(temp)
-
-print(not_friendly)
+# 최소 사진 찍는 횟수 계산
+result = min_photos(N, dislikes)
+print(result)
