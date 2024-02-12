9fabc73e84bf93ad79b49dfc93d899d4336929e2
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-12 22:34:10 +0900

    Title: 스승의 은혜, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240212/스승의 은혜/the-grace-form-teacher.py
+++ 240212/스승의 은혜/the-grace-form-teacher.py
@@ -9,13 +9,15 @@ for i in range(N):
 presents.sort(key=lambda x: (x[0] + x[1], x[0]))  # 가격 + 배송비가 적은 순서대로 정렬
 
 count = 0
+coupon_used = False  # 쿠폰 사용 여부를 나타내는 변수 추가
 for price, delivery in presents:
     if price <= B:
         B -= price
         count += 1
-    elif price // 2 + delivery <= B:  # 쿠폰을 사용하는 경우
+    elif not coupon_used and price // 2 + delivery <= B:  # 쿠폰을 사용하여 예산 내에서 선물을 구매하는 경우
         B -= price // 2 + delivery
         count += 1
+        coupon_used = True  # 쿠폰을 사용했으므로 True로 설정
     else:
         break
 
