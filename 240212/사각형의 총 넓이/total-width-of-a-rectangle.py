d0261d352f7827f471e0d8a6d76848d61aa3e0fc
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-12 22:47:16 +0900

    Title: 사각형의 총 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240212/사각형의 총 넓이/total-width-of-a-rectangle.py
@@ -0,0 +1,22 @@
+N = int(input())
+
+base = [[0] * 1001 for i in range(1001)]
+
+OFFSET = 500
+
+for i in range(N):
+    x1, y1, x2, y2 = map(int, input().split())
+    x1 += OFFSET
+    y1 += OFFSET
+    x2 += OFFSET
+    y2 += OFFSET
+    for i in range(y2, y1):
+        for j in range(x1, x2):
+            base[i][j] = 1
+cnt = 0
+
+for i in base:
+    for j in i:
+        if j >= 1:
+            cnt += 1
+print(cnt)
