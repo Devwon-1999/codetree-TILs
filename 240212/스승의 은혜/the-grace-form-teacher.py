2b59e10e362490d193707f208e4bccff18af02f2
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-12 22:37:31 +0900

    Title: 스승의 은혜, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 240212/스승의 은혜/the-grace-form-teacher.py
+++ 240212/스승의 은혜/the-grace-form-teacher.py
@@ -4,20 +4,23 @@ presents = []
 
 for i in range(N):
     price, delivery = map(int, input().split())
-    presents.append((price + delivery, price))  # 선물 가격과 배송비를 더한 값과 선물 가격만 저장
+    presents.append((price, delivery))
 
-presents.sort()  # 가격 + 배송비가 적은 순서대로 정렬
+presents.sort(key=lambda x: (x[0] + x[1], x[0]))  # 가격 + 배송비가 적은 순서대로 정렬
 
-count = 0
-for price_plus_delivery, price in presents:
-    if price <= B:  # 현재 학생의 선물 가격이 예산 내에 있는 경우
-        B -= price  # 예산에서 선물 가격을 차감
-        count += 1  # 선물을 준 학생 수 증가
-    elif B >= price // 2:  # 절반 가격으로 선물을 구매할 수 있는 경우
-        B -= price // 2  # 절반 가격을 지불하고 선물을 줌
-        count += 1  # 선물을 준 학생 수 증가
-        break  # 더 이상 선물을 줄 수 없음
-    else:
-        break  # 선물을 구매할 예산이 부족한 경우 루프 탈출
+max_students = 0
+for i in range(N):
+    total_cost = 0
+    students = 0
+    for j in range(N):
+        if j == i:  # 쿠폰 사용 대상 학생
+            if total_cost + presents[j][0] // 2 + presents[j][1] <= B:
+                total_cost += presents[j][0] // 2 + presents[j][1]
+                students += 1
+        elif total_cost + presents[j][0] + presents[j][1] <= B:
+            total_cost += presents[j][0] + presents[j][1]
+            students += 1
 
-print(count)
+    max_students = max(max_students, students)
+
+print(max_students)
