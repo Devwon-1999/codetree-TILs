9606998848bae6f5ffe7afc42cb6abc339e8b4aa
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-12 22:36:33 +0900

    Title: 스승의 은혜, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240212/스승의 은혜/the-grace-form-teacher.py
+++ 240212/스승의 은혜/the-grace-form-teacher.py
@@ -4,21 +4,20 @@ presents = []
 
 for i in range(N):
     price, delivery = map(int, input().split())
-    presents.append((price, delivery))
+    presents.append((price + delivery, price))  # 선물 가격과 배송비를 더한 값과 선물 가격만 저장
 
-presents.sort(key=lambda x: (x[0] + x[1], x[0]))  # 가격 + 배송비가 적은 순서대로 정렬
+presents.sort()  # 가격 + 배송비가 적은 순서대로 정렬
 
 count = 0
-coupon_used = False  # 쿠폰 사용 여부를 나타내는 변수 추가
-for price, delivery in presents:
-    if price <= B:
-        B -= price
-        count += 1
-    elif not coupon_used and price // 2 + delivery <= B:  # 쿠폰을 사용하여 예산 내에서 선물을 구매하는 경우
-        B -= price // 2 + delivery
-        count += 1
-        coupon_used = True  # 쿠폰을 사용했으므로 True로 설정
+for price_plus_delivery, price in presents:
+    if price <= B:  # 현재 학생의 선물 가격이 예산 내에 있는 경우
+        B -= price  # 예산에서 선물 가격을 차감
+        count += 1  # 선물을 준 학생 수 증가
+    elif B >= price // 2:  # 절반 가격으로 선물을 구매할 수 있는 경우
+        B -= price // 2  # 절반 가격을 지불하고 선물을 줌
+        count += 1  # 선물을 준 학생 수 증가
+        break  # 더 이상 선물을 줄 수 없음
     else:
-        break
+        break  # 선물을 구매할 예산이 부족한 경우 루프 탈출
 
 print(count)
