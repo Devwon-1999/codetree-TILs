339bf16d1b5bdf193101f9154f3d2762c8959b33
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-13 21:40:19 +0900

    Title: 숨은 단어 찾기 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240213/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 240213/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -9,6 +9,20 @@ for i in range(N):
         temp.append(j)
     base.append(temp)
 
-
-for i in base:
-    print(i)
+def in_range(x, y):
+    return 0 <= x and x < M and 0 <= y and y < N
+    
+dxys = [
+    #        가로                  세로                     대각선
+    [[0, 1, 2],[0, 0, 0]], [[0, 0, 0],[0, 1, 2]], [[0, 1, 2],[0, 1, 2]], 
+    [[0, -1, -2],[0, 0, 0]], [[0, 0, 0],[0, -1, -2]], [[0, -1, -2],[0, -1, -2]]
+]
+for i in range(N):
+    for j in range(M):
+        if base[i][j] == "L":
+            print(base[i][j], end = " ")
+            for dxs, dys in dxys:
+                for dx, dy in zip(dxs, dys):
+                    if in_range(i + dx, i + dy) and base[dx][dy] == "E":
+                        print(base[dx][dy], end = " ")
+            print()
