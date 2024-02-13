b40318324c5bf1a56909e7f8868fd2f069d9508c
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-13 21:02:41 +0900

    Title: 오목, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240213/오목/O-mok.py
@@ -0,0 +1,59 @@
+base = []
+
+#가로 
+widthx, widthy = [0, 1, 2, 3, 4], [0, 0, 0, 0, 0]
+#세로
+lengthx, lengthy = [0, 0, 0, 0, 0], [0, 1, 2, 3, 4]
+#대각선
+diagonalx, diagonaly = [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]
+
+for i in range(19):
+    temp = list(map(int, input().split()))
+    base.append(temp)
+result = []
+for i in range(19 - 4):
+    for j in range(19 - 4):
+        if base[j][i] == 1:
+            temp = []
+            for x, y in zip(widthx, widthy):
+                if base[j + y][i + x] == 1:
+                    temp.append([j + y + 1, i + x + 1])
+                else:
+                    break
+
+            for x, y in zip(lengthx, lengthy):
+                if base[j + y][i + x] == 1:
+                    temp.append([j + y + 1, i + x + 1])
+                else:
+                    break
+
+            for x, y in zip(diagonalx, diagonaly):
+                if base[j + y][i + x] == 1:
+                    temp.append([j + y + 1, i + x + 1])
+                else:
+                    break
+            if len(temp) == 5:
+                result.append(temp)
+        
+        elif base[j][i] == 2:
+            for x, y in zip(widthx, widthy):
+                if base[j + y][i + x] == 1:
+                    temp.append([j + y + 1], [i + x + 1])
+                else:
+                    break
+
+            for x, y in zip(lengthx, lengthy):
+                if base[j + y][i + x] == 1:
+                    temp.append([j + y + 1, i + x + 1])
+                else:
+                    break
+
+            for x, y in zip(diagonalx, diagonaly):
+                if base[j + y][i + x] == 1:
+                    temp.append([j + y + 1, i + x + 1])
+                else:
+                    break
+            if len(temp) == 5:
+                result.append(temp)
+for i in result:
+    print(i)
