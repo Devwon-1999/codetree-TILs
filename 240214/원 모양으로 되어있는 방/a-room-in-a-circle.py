28a1b8654fb38b5d40e54e2801dceb97202816d9
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-14 10:08:07 +0900

    Title: 원 모양으로 되어있는 방, Time: 182ms, Memory: 25MB, Status: Passed - CodeTree

--- 240214/원 모양으로 되어있는 방/a-room-in-a-circle.py
+++ 240214/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -1,3 +1,7 @@
+import sys
+INT_MAX = sys.maxsize
+min_val = INT_MAX
+
 N = int(input()) # 방의 수 입력
 
 bang = [] # 방별 수용 인원 수
@@ -6,8 +10,7 @@ for i in range(N): # 방별 수용 인원 수 입력
     temp = int(input())
     bang.append(temp)
 
-min_val = 1000000
-
+#출발지 별 결과
 for i in range(N):
     distance = 0
     for j in range(N): #현재 방의 위치부터 전체 방까지 반복
