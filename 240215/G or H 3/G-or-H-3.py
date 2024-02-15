bde7937416111ce8fdb4db05a22fa5c58e24dd35
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-15 21:30:21 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240215/G or H 3/G-or-H-3.py
@@ -0,0 +1,22 @@
+N, K = map(int, input().split())
+#N명의 사람, K의 사진크기
+
+base = [0 for i in range(101)] #총 100개의 위치
+
+for i in range(N): 
+    place, alpha = input().split() #위치와 알파벳
+    place = int(place)
+    base[place - 1] = alpha
+
+result = []
+for i in range(N):
+    temp = 0
+    for j in range(i, i + K + 1):
+        if base[j] == 0:
+            pass
+        elif base[j] == "G":
+            temp += 1
+        elif base[j] == "H":
+            temp += 2
+    result.append(temp)
+print(max(result))
