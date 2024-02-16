13d9d6a8316b631c10f863db24a7de3ddba55381
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-16 22:02:48 +0900

    Title: 바구니 안의 사탕 2, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240216/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240216/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -3,19 +3,19 @@ base = [0 for i in range(101)]
 index_List = []
 for i in range(N): # 위치, 사탕 개수 셋팅
     candy, index = map(int, input().split())
-    if base[index-1] == 0:
-        base[index - 1] = candy
-    else:
-        base[index - 1] += candy
-    print(base[index - 1])
+    base[index - 1] += candy
+
 
 result = [] #결과 리스트
-for i in range(N): #메인 알고리즘
+for i in range(101): #메인 알고리즘
     candy_cnt = 0
-    if i - K < 0 and i + K >= 100:
-        continue
+
+        
     for j in range(i - K, i + K + 1): # 중심점으로부터 C - K ~ C + K 까지
+
+        if j <= len(base) - 1 and j >= 0:
             candy_cnt += base[j]
+
     result.append(candy_cnt)
 
-print(result)
+print(max(result))
