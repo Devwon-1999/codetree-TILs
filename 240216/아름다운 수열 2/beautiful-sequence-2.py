b8577ccb6c4ac4f6168e509895feb5ce2bf4ae96
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-16 22:07:51 +0900

    Title: 아름다운 수열 2, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 240216/아름다운 수열 2/beautiful-sequence-2.py
+++ 240216/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,26 +1,27 @@
-# 순열 생성
-def generate_permutations(arr, start, end, result):
-    if start == end:
-        result.append(arr[:])
-    else:
-        for i in range(start, end):
-            arr[start], arr[i] = arr[i], arr[start]
-            generate_permutations(arr, start + 1, end, result)
-            arr[start], arr[i] = arr[i], arr[start]
-
-
+# 입력 받기
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-permutations_B = []
-generate_permutations(B, 0, M, permutations_B)
+# B의 각 숫자가 몇 개씩 있는지를 저장
+counts_B = {}
+for num in B:
+    if num in counts_B:
+        counts_B[num] += 1
+    else:
+        counts_B[num] = 1
 
-count = 0 # 정답
+count = 0
 
 for i in range(N - M + 1): #부분 수열 확인
     subsequence = A[i:i+M]
-    if subsequence in permutations_B:
+    counts_subsequence = {}
+    for num in subsequence:
+        if num in counts_subsequence:
+            counts_subsequence[num] += 1
+        else:
+            counts_subsequence[num] = 1
+    if counts_subsequence == counts_B:
         count += 1
 
 print(count)
