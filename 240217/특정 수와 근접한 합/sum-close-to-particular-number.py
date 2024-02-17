cf5dac761f73d62d9c534ae8958bd56a0f3670a2
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-17 16:31:57 +0900

    Title: 특정 수와 근접한 합, Time: 132ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240217/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -0,0 +1,20 @@
+N, S = map(int, input().split())
+numList = list(map(int, input().split()))
+
+allList = []
+
+# 2개의 원소를 제외한 모든 조합 생성
+for i in range(len(numList)):
+    for j in range(i+1, len(numList)):
+        combination = [numList[k] for k in range(len(numList)) if k != i and k != j]
+        allList.append(combination)
+
+sumList = []
+for i in allList:
+	sumList.append(sum(i))
+
+subList = []
+for i in sumList:
+	subList.append(i - S)
+
+print(min(subList))
