e35a49791244d2e277eca62ea76edeb2da9eac97
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-18 21:41:11 +0900

    Title: 최고의 13위치 2, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240218/최고의 13위치 2/best-place-of-13-2.py
+++ 240218/최고의 13위치 2/best-place-of-13-2.py
@@ -6,19 +6,20 @@ for i in range(N):
     temp = list(map(int, input().split()))
     base.append(temp)
 
-value = []
+max_cnt = 0
 for i in range(N - 1):
     for j in range(N - 2):
-        temp = 0
+        temp1 = 0
+        temp2 = 0
         for k in range(3):
-            temp += base[i][j + k]
-        value.append(temp)
-if N > 6:
-    max_value1 = max(value)
-    value.remove(max_value1)
-    max_value2 = max(value)
-    value.remove(max_value2)
-    print(max_value1 + max_value2)
-else:
-    max_value1 = max(value)
-    print(max_value1)
+            temp1 += base[i][j + k]
+            temp2 += base[i + 1][j + k]
+        max_cnt = max(max_cnt, temp1 + temp2)
+if N >= 6:     
+    for i in range(N):
+        for j in range(N - 5):
+            temp = 0
+            for k in range(6):
+                temp += base[i][j + k]
+            max_cnt = max(max_cnt, temp)
+print(max_cnt)
