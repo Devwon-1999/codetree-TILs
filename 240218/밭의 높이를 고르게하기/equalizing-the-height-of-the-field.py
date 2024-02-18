6d33e3c613f92e86fcfe55d7b6f523172a254467
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-18 23:27:58 +0900

    Title: 밭의 높이를 고르게하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240218/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240218/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -4,7 +4,25 @@ N, H, T = map(int, input().split())
 # T 번
 height_List = list(map(int, input().split()))
 
+
+piece_List = []
 for i in range(N - T + 1):
+    temp = []
     for j in range(i, i + T):
-        print(i, j)
-    print()
+        temp.append(height_List[j])
+    piece_List.append(temp)
+
+difference_List = []
+for i in piece_List:
+    temp = []
+    for j in i:
+        if j != H:
+            temp.append(abs(j - H))
+        else:
+            temp.append(0)
+    difference_List.append(temp)
+
+min_val = 10000000000
+for i in difference_List:
+    min_val = min(sum(i), min_val)
+print(min_val)
