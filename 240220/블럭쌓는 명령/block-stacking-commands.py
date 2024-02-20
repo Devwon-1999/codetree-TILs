849bc71dcb9b592e1c1e8c1f577d16660a8d342a
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-20 22:52:41 +0900

    Title: 블럭쌓는 명령, Time: 5041ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

--- 240220/블럭쌓는 명령/block-stacking-commands.py
+++ 240220/블럭쌓는 명령/block-stacking-commands.py
@@ -6,7 +6,7 @@ for _ in range(K):
     a, b = map(int, input().split())
     for i in range(a, b + 1):
         blocks[i] += 1
-
+blocks.pop(0)
 sorted_blocks = sorted(blocks)
 mid_index = N // 2
 print(sorted_blocks[mid_index])
