4bf72326027adbf9c771aa4f3bb1cf9cebbebcad
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-20 22:52:07 +0900

    Title: 블럭쌓는 명령, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240220/블럭쌓는 명령/block-stacking-commands.py
+++ 240220/블럭쌓는 명령/block-stacking-commands.py
@@ -1,13 +1,12 @@
 N, K = map(int, input().split())
-base = [0 for i in range(N + 1)]
 
-for i in range(K):
-    x, y = map(int, input().split())
-    for i in range(x, y + 1):
-        base[i] += 1
+blocks = [0] * (N + 1)
 
-base.pop(0)
-base = sorted(base)
+for _ in range(K):
+    a, b = map(int, input().split())
+    for i in range(a, b + 1):
+        blocks[i] += 1
 
-center = N // 2
-print(base[center])
+sorted_blocks = sorted(blocks)
+mid_index = N // 2
+print(sorted_blocks[mid_index])
