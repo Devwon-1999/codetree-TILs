7d861fb7cd5c1772cff0a8d3c880209259b66b29
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-20 22:48:17 +0900

    Title: 블럭쌓는 명령, Time: 5097ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

--- 240220/블럭쌓는 명령/block-stacking-commands.py
+++ 240220/블럭쌓는 명령/block-stacking-commands.py
@@ -1,11 +1,12 @@
 N, K = map(int, input().split())
-base = [0 for i in range(N)]
+base = [0 for i in range(N + 1)]
 
 for i in range(K):
     x, y = map(int, input().split())
     for i in range(x, y + 1):
         base[i] += 1
 
+base.pop(0)
 base.sort()
 
 center = N // 2
