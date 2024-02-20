9dc05dfbdcf1334c9e145d766d54bb4f780c6e0a
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-20 22:55:45 +0900

    Title: 블럭쌓는 명령, Time: 156ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240220/블럭쌓는 명령/block-stacking-commands.py
+++ 240220/블럭쌓는 명령/block-stacking-commands.py
@@ -1,12 +1,22 @@
+from collections import defaultdict
+
 N, K = map(int, input().split())
 
-blocks = [0] * (N + 1)
+commands = defaultdict(int)  # 명령의 범위를 기록할 딕셔너리
 
 for _ in range(K):
     a, b = map(int, input().split())
-    for i in range(a, b + 1):
-        blocks[i] += 1
-blocks.pop(0)
+    commands[a] += 1
+    commands[b + 1] -= 1  # 범위의 끝 다음 인덱스에 -1을 기록하여 범위를 표시
+
+blocks = [0] * (N + 1)  # 블록의 높이를 기록할 배열
+current_blocks = 0  # 현재 높이의 블록 수
+mid_index = N // 2  # 중앙 인덱스
+
+for i in range(1, N + 1):
+    current_blocks += commands[i]
+    blocks[i] = current_blocks
+
+# 블록의 높이 배열을 정렬하고 중앙 값을 출력
 sorted_blocks = sorted(blocks)
-mid_index = N // 2
 print(sorted_blocks[mid_index])
