ba06235bfd7785c6e6ef33851090bac1c2054881
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-21 21:46:00 +0900

    Title: 문자 사각형 출력하기 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240221/문자 사각형 출력하기 2/print-char-rectangle-2.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+alpha = [[0] * n for i in range(n)]
+dxs, dys = [0, 1, 0, -1] , [1, 0, -1, 0]
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+num = 65
+for y in range(n):
+    for x in range(n):
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if in_range(nx, ny) and alpha[nx][ny] == 0:
+                alpha[nx][ny] = num
+        num += 1
+        
+for i in alpha:
+    for j in i:
+        print(chr(j), end = " ")
+    print()
