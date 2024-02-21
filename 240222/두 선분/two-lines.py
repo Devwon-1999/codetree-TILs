7bb72fcdb3b39697b8ef05e55dc987f6de819f9c
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-22 03:18:46 +0900

    Title: 두 선분, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240222/두 선분/two-lines.py
+++ 240222/두 선분/two-lines.py
@@ -1,10 +1,9 @@
 x1, x2, x3, x4 = map(int, input().split())
 
-answer = "nonintersecting"
+answer = "intersecting"
 
-for i in range(x1, x2 + 1):
-    for j in range(x3, x4 + 1):
-        if i == j:
-            answer = "intersecting"
+
+if x2 < x3 or x4 < x1:
+    answer = "nonintersecting"
             
 print(answer)
