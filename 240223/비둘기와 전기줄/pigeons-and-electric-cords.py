a4f679018b69af59ecfe417ee2922da70050a49c
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-23 23:36:39 +0900

    Title: 비둘기와 전기줄, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240223/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 240223/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -1,5 +1,15 @@
 N = int(input())
-print(N)
+base = [0 for i in range(11)]
 for i in range(N):
     a, b = map(int, input().split())
-    print(a, b)
+    base[a - 1] += 1
+
+answer = 0
+for i in base:
+    cnt = 0
+    for j in range(i):
+        cnt += 1
+    if cnt % 2 == 0:
+        answer += cnt / 2
+
+print(int(answer))
