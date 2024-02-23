2d5b314263c8afca6e9c3644661cf30d2ddf99d6
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-23 23:15:24 +0900

    Title: 전부 겹치는 선분, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240223/전부 겹치는 선분/overlapping-line-segments.py
+++ 240223/전부 겹치는 선분/overlapping-line-segments.py
@@ -3,15 +3,16 @@ N = int(input())
 base = [0 for i in range(101)]
 for i in range(N):
     x1, x2 = map(int, input().split())
-    for j in range(x1, x2):
+    for j in range(x1, x2 + 1):
         base[j] += 1
 check = False
 for i in base:
-    if i == 0 or i == 1:
-        continue
-    else:
+    if i == N:
         check = True
         break
+    else:
+        continue
+
 if check:
     print("Yes")
 else:
