dca81d58fb589b1467b4a031a10dc907db0f6223
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-24 19:08:21 +0900

    Title: 비둘기와 전기줄, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240224/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 240224/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -1,10 +1,10 @@
-N = int(input())
-base = [0 for i in range(11)]
-place = [-1 for i in range(11)]
-cnt = 0
-for i in range(N):
-    a, b = map(int, input().split())
-    base[a - 1] += 1
+N = int(input()) # 비둘기가 총 이동했던 경우 N번 입력
+
+place = [-1 for i in range(11)] # 비둘기들의 현재 위치
+cnt = 0 #정답을 기록할 cnt(카운트, 움직인 횟수 계산시 사용할) 변수
+
+for i in range(N): #반복문 N번 반복
+    a, b = map(int, input().split()) #a 비둘기, b 위치
     if place[a - 1] == -1:
         place[a - 1] = b
     elif place[a - 1] == 1:
