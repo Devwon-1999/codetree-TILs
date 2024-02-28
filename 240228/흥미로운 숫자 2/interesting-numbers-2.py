fadc097dfd7c62d9fe60afd59eab181810ce3a7d
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-02-28 17:48:49 +0900

    Title: 흥미로운 숫자 2, Time: 247ms, Memory: 25MB, Status: Passed - CodeTree

--- 240228/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240228/흥미로운 숫자 2/interesting-numbers-2.py
@@ -3,10 +3,14 @@ cnt = 0
 for i in range(X, Y + 1):
     i = str(i)
     temp = []
+    check = set()
     for j in i:
         if j not in temp:
             temp.append(j)
-    if len(temp) == 2:
+        else:
+            check.add(j)
+
+    if len(temp) == 2 and len(check) == 1:
         cnt += 1
 
 print(cnt)
