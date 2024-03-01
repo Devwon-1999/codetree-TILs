e02d6153dd1e8dfd2ae41bbd4b37c9b7ad7fed57
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-03-01 22:15:48 +0900

    Title: 운행 되고 있는 시간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240301/운행 되고 있는 시간/hours-in-service.py
@@ -0,0 +1,25 @@
+N = int(input())
+
+worktime = []
+for i in range(N):
+    start, end = map(int, input().split())
+    worktime.append([start, end])
+
+fire_1 = []
+for i in range(N):
+    fire_1.append(worktime[:i] + worktime[i + 1:])
+
+answer = []
+for i in fire_1:
+    temp = [0 for i in range(50)]
+    for j in i:
+        for time in range(j[0], j[1] + 1):
+            temp[time] += 1
+
+    cnt = 0
+    for i in temp:
+        if i != 0:
+            cnt += 1
+    answer.append(cnt)
+
+print(min(answer))
