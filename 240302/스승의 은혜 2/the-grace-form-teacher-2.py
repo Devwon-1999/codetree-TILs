81f8e3a5602e2e17eaf6f3d72baba4100b23443f
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-03-02 03:50:05 +0900

    Title: 스승의 은혜 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240302/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,17 @@
+N, B = map(int, input().split()) #N명, B예산
+
+price_List = []
+for i in range(N):
+    price = int(input())
+    price_List.append(price)
+
+price_List.sort()
+
+sum_price = 0
+cnt = 0
+for i in price_List:
+    if B > sum_price:
+        sum_price += i
+        cnt += 1
+
+print(cnt)
