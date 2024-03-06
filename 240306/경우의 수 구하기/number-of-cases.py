310621a2bc6cd097af9fc6776ed1a25fa5cb9665
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-03-06 21:48:19 +0900

    Title: 경우의 수 구하기, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240306/경우의 수 구하기/number-of-cases.py
+++ 240306/경우의 수 구하기/number-of-cases.py
@@ -12,5 +12,11 @@ for i in range(n):
     else:
         dic[group] = thing
 
-print(dic)
-print(type(dic))
+result = 1
+for group in dic.values():
+    if isinstance(group, list):
+        result *= (len(group) + 1)
+    else:
+        result *= 2
+
+print(result - 1)
