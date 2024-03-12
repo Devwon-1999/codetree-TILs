95b245cbf42cd28b18aa0f4a819ee70d79dca753
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-03-12 18:47:47 +0900

    Title: 황금비율 토스트, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240312/황금비율 토스트/golden-toast.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+
+password = list(input())
+index = n
+for i in range(m):
+    input_val = list(input().split())
+
+    if input_val[0] == "L":
+        if index == 0:
+            continue
+        else:
+            index -= 1
+    elif input_val[0] == "R":
+        if index == n - 1:
+            continue
+        else:
+            index += 1
+
+    elif input_val[0] == "D":
+        if index == n:
+            continue
+        else:
+            password.pop(index)
+    
+    elif input_val[0] == "P":
+        password.insert(index, input_val[1])
+
+for i in password:
+    print(i, end = "")
