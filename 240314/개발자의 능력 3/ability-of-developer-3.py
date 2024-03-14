6bc03a907d78fee8639be77ab27c84982735fc8b
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-03-14 15:20:56 +0900

    Title: 개발자의 능력 3, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240314/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,14 @@
+numList = list(map(int, input().split()))
+
+def get_diff(i, j, k):
+    sum1 = numList[i] + numList[j] + numList[k]
+    sum2 = sum(numList) - sum1
+    return abs(sum1 - sum2)
+
+min_diff = 99999999999
+for i in range(6):
+    for j in range(i + 1, 6):
+        for k in range(j + 1, 6):
+            min_diff = min(min_diff, get_diff(i, j, k))
+
+print(min_diff)
