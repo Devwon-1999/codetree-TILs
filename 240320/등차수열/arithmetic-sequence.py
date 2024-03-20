8e46cc123d02f4fa19293439c8b80f62c6dc1799
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-03-20 22:19:50 +0900

    Title: 등차수열, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240320/등차수열/arithmetic-sequence.py
+++ 240320/등차수열/arithmetic-sequence.py
@@ -6,7 +6,7 @@ answer=0
 for k in range(1, 101):
     cnt=0
     for i in range(n):
-        for j in range(i, n): 
+        for j in range(i + 1, n): 
             if (numList[i] + numList[j]) / 2 == k:
                 cnt+=1
     answer = max(answer, cnt)
