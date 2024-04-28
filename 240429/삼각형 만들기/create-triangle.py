7d01ab609d0ef03a1b8b9fefa365eff91d1c9fa2
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-04-29 07:13:13 +0900

    Title: 삼각형 만들기, Time: 145ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240429/삼각형 만들기/create-triangle.py
@@ -0,0 +1,36 @@
+N = int(input())
+
+points = []
+for i in range(N):
+    x, y = map(int, input().split())
+    points.append([x, y])
+
+# print(points)
+
+max_val = 0
+
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        
+        for k in range(j + 1, len(points)):
+            cnt = 0
+            if points[i][0] - points[j][0] == 0:
+                cnt += 1
+            if points[j][0] - points[k][0] == 0:
+                cnt += 1
+            if points[i][0] - points[k][0] == 0:
+                cnt += 1
+            if points[i][1] - points[j][1] == 0:
+                cnt += 1
+            if points[j][1] - points[k][1] == 0:
+                cnt += 1
+            if points[i][1] - points[k][1] == 0:
+                cnt += 1
+
+            if cnt == 2:
+                area = abs((points[i][0] * points[j][1]) + (points[j][0] * points[k][1]) + (points[k][0] * points[i][1]) - 
+                            (points[i][1] * points[j][0]) + (points[j][1] * points[k][0]) + (points[k][1] * points[i][0]))
+                max_val = max(max_val, area)
+            # print(points[i], points[j], points[k])
+            # print(cnt)
+print(max_val)
