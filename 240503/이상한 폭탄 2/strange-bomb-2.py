48515041715d46cc3d8ee6aeaa3d4855606831e5
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-05-03 23:24:46 +0900

    Title: 이상한 폭탄 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240503/이상한 폭탄 2/strange-bomb-2.py
+++ 240503/이상한 폭탄 2/strange-bomb-2.py
@@ -5,11 +5,16 @@ for i in range(N):
     base.append(temp)
 answer = -1
 
-for i in range(N - K + 1):
+for i in range(N - K):
     for j in range(i + 1, i + K + 1):
-        if base[i] == base[j - 1]:
+        if base[i] == base[j]:
             answer = base[i]
-        else:
-            continue
-
+if N == K:
+    for i in range(N):
+        cnt = 0
+        for j in base:
+            if i == j:
+                cnt += 1
+        if cnt == 2:
+            answer = i
 print(answer)
