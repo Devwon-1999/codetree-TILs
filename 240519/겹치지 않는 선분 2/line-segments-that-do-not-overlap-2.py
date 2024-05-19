11768c28c4985def84ba77e16bcf90c321976f79
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-05-19 22:43:38 +0900

    Title: 겹치지 않는 선분 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240519/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+
+base = [0 for i in range(2000001)]
+for i in range(N):
+    x1, x2 = map(int, input().split())
+    x1 += OFFSET
+    x2 += OFFSET
+    for j in range(x1, x2):
+        base[j] += 1
+
+cnt = 0
+for i in base:
+    if i == 1:
+        cnt += 1
+print(cnt)
