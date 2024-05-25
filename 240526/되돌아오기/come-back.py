4d959db655a7d0b0f9228539b4dc0067f5624117
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-05-26 07:39:32 +0900

    Title: 되돌아오기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240526/되돌아오기/come-back.py
@@ -0,0 +1,34 @@
+N = int(input())
+
+x, y = 0, 0
+dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1] #동북남서
+dir_num = 0
+time = 0
+result = 0
+for i in range(N):
+    direction, distance = input().split()
+    distance = int(distance)
+
+    if direction == "E":
+        dir_num = 0
+    elif direction == "N":
+        dir_num = 1
+    elif direction == "S":
+        dir_num = 2
+    elif direction == "W":
+        dir_num = 3
+        
+    for i in range(distance):
+        time += 1
+        x, y = x + dxs[dir_num], y + dys[dir_num]
+        if x == 0 and y == 0:
+            result = time
+            break
+    if x == 0 and y == 0:
+        break
+    
+        
+if result:
+    print(result)
+else:
+    print(-1)
