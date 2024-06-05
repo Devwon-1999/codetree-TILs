a1de12b5571240aad63de69179f7c1405e1ebcf8
Devwon-1999 <147505682+Devwon-1999@users.noreply.github.com>
2024-06-05 23:29:38 +0900

    Title: 씨 오 더블유 2, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240605/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,14 @@
+N = int(input())
+string = input()
+
+result = []
+
+for i in range(N):
+    if string[i] == "C":
+        for j in range(i, N):
+            if string[j] == "O":
+                for k in range(j, N):
+                    if string[k] == "W":
+                        result.append([i+1, j+1, k+1])
+
+print(len(result))
